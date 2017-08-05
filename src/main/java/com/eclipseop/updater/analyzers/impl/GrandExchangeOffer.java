f477aa260f1344e9f135fc211838ed30a61ac236
Eclipseop <guest173800@gmail.com>
2017-08-05 11:03:31 -0400

    GEOffer hooks

--- src/main/java/com/eclipseop/updater/analyzers/impl/GrandExchangeOffer.java
+++ src/main/java/com/eclipseop/updater/analyzers/impl/GrandExchangeOffer.java
@@ -3,10 +3,9 @@ package com.eclipseop.updater.analyzers.impl;
 import com.eclipseop.updater.analyzers.Analyzer;
 import com.eclipseop.updater.util.found_shit.FoundClass;
 import com.eclipseop.updater.util.found_shit.FoundField;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
 
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 
 /**
@@ -19,7 +18,8 @@ public class GrandExchangeOffer extends Analyzer {
 	public FoundClass identifyClass(ArrayList<ClassNode> classNodes) {
 		for (ClassNode classNode : classNodes) {
 			if (classNode.fieldCount("B", true) == 1 && classNode.fieldCount("I", true) == 5) {
-				return new FoundClass(classNode, "GrandExchangeOffer").addExpectedField("metadata");
+				return new FoundClass(classNode, "GrandExchangeOffer")
+						.addExpectedField("metadata", "itemId", "itemPrice", "itemQuantity", "amountTransferred", "amountSpent");
 			}
 		}
 
@@ -28,13 +28,42 @@ public class GrandExchangeOffer extends Analyzer {
 
 	@Override
 	public void findHooks(FoundClass foundClass) {
-		for (FieldNode fieldNode : foundClass.getRef().fields) {
-			if (Modifier.isStatic(fieldNode.access)) {
+
+		int found = 0;
+		hooks:
+		for (MethodNode methodNode : foundClass.getRef().methods) {
+			if (!methodNode.name.equals("<init>")) {
 				continue;
 			}
 
-			if (fieldNode.desc.equals("B")) {
-				foundClass.addFields(new FoundField(fieldNode, "metadata"));
+			AbstractInsnNode ain = methodNode.instructions.getFirst();
+			while ((ain = ain.getNext()) != null) {
+				if (found >= 6) {
+					break hooks;
+				}
+
+				if (ain.opcode() == Opcodes.GOTO) {
+					ain = ((JumpInsnNode) ain).label.getNext();
+				} else if (ain instanceof FieldInsnNode) {
+					found++;
+
+					String name = null;
+					if (found == 1) {
+						name = "metadata";
+					} else if (found == 2) {
+						name = "itemId";
+					} else if (found == 3) {
+						name = "itemPrice";
+					} else if (found == 4) {
+						name = "itemQuantity";
+					} else if (found == 5) {
+						name = "amountTransferred";
+					} else if (found == 6) {
+						name = "amountSpent";
+					}
+
+					foundClass.addFields(new FoundField(foundClass.findField((FieldInsnNode) ain), name));
+				}
 			}
 		}
 	}
